c36a937e75234679ef4b128a38828fc008a2df5f
Juan Carlos Bajan <juanca@olivegt.com>
2025-02-18 16:57:39 -0600

    changes

--- models/pos_order.py
+++ models/pos_order.py
@@ -1,12 +1,18 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class PosOrder(models.Model):
     _inherit = 'pos.order'
 
-    to_invoice = fields.Boolean(default=True)  # Asegura que siempre se genere factura
+    to_invoice = fields.Boolean(default=True)  # Siempre forzar facturación
 
-    def _prepare_invoice_vals(self):
-        """Forzar siempre la generación de factura"""
-        vals = super(PosOrder, self)._prepare_invoice_vals()
-        vals['invoice_origin'] = self.name
-        return vals
+    @api.model
+    def create(self, vals):
+        """Asegurar que todas las órdenes se creen con to_invoice=True"""
+        vals['to_invoice'] = True
+        return super(PosOrder, self).create(vals)
+
+    def write(self, vals):
+        """Evita que to_invoice sea cambiado a False"""
+        if 'to_invoice' in vals and not vals['to_invoice']:
+            vals['to_invoice'] = True
+        return super(PosOrder, self).write(vals)
--- static/src/js/force_invoice.js
+++ static/src/js/force_invoice.js
@@ -6,15 +6,20 @@ import { Order } from "@point_of_sale/app/store/models";
 patch(Order.prototype, {
     setup() {
         this._super.apply(this, arguments);
-        this.to_invoice = true; // Forzar siempre la facturación
+        this.to_invoice = true; // Mantener la facturación activada
     },
 
     is_to_invoice() {
-        return true; // Siempre devuelve true, obligando a la facturación
+        return true; // Asegurar que siempre esté activo
     },
 
     toggleIsToInvoice() {
-        // Evita que el usuario pueda desactivar la opción de facturación
+        // Evita que el usuario pueda apagar la opción
         return;
+    },
+
+    set_to_invoice(value) {
+        // Forzar siempre to_invoice en true
+        this.to_invoice = true;
     }
 });

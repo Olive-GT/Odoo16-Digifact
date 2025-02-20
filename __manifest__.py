a64b76cad59f7ef767ee6478d2d33891a438790c
Juan Carlos Bajan <juanca@olivegt.com>
2025-02-20 12:15:06 -0600

    mail template

--- __manifest__.py
+++ __manifest__.py
@@ -11,6 +11,7 @@
         'views/res_company_views.xml',
         'views/pos_order_views.xml',
         'views/pos_config_views.xml',
+        'data/mail_temnplate.xml',
         ],
     'assets': {
         'point_of_sale._assets_pos': [
